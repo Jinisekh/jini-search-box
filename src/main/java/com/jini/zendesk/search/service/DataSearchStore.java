d6a02d054c976582c2bf9cc15d7684703353e7d0
jini <jinisekh87@gmail.com>
2021-03-03 08:21:34 +1100

    Refactoring

--- src/main/java/com/jini/zendesk/search/service/DataSearchStore.java
+++ src/main/java/com/jini/zendesk/search/service/DataSearchStore.java
@@ -9,37 +9,24 @@ import java.util.Set;
 
 public class DataSearchStore {
 
+    public static final String PRINT_LINE = "-------------------------------------------------------";
+
     public void invokeSearchService(DataIndexStore indexStore) {
         System.out.println("Select 1) Users or 2) Tickets or 3) Organizations\n");
         Scanner in = new Scanner(System.in);
-        String input = "";
-        input = in.next();
-        Scanner scan;
-        String userInputField;
-        String userInputValue;
+        String input = in.nextLine();
+        System.out.println("Enter the search term::");//-> Extract this to a method
+        String userInputField  = in.nextLine();
+        System.out.println("Enter the search value::");
+        String userInputValue = in.nextLine();
         switch (input) {
             case "1":
-                scan = new Scanner(System.in);
-                System.out.println("Enter the search term::");
-                userInputField  = scan.nextLine();
-                System.out.println("Enter the search value::");
-                userInputValue = scan.nextLine();
                 displayUserResult(userInputField,userInputValue,indexStore);
                 break;
             case "2":
-                scan = new Scanner(System.in);
-                System.out.println("Enter the search term::");
-                userInputField  = scan.nextLine();
-                System.out.println("Enter the search value::");
-                userInputValue = scan.nextLine();
                 displayTicketResult(userInputField,userInputValue,indexStore);
                 break;
             case "3":
-                scan = new Scanner(System.in);
-                System.out.println("Enter the search term::");
-                userInputField  = scan.nextLine();
-                System.out.println("Enter the search value::");
-                userInputValue = scan.nextLine();
                 displayOrgResult(userInputField,userInputValue,indexStore);
                 break;
             default:
@@ -53,16 +40,21 @@ public class DataSearchStore {
         Map<Integer,Set<String>> orgToTicketRelationMap = indexStore.getOrgToTicketRelationMap();
         Map<Integer,Set<Integer>> orgToUserRelationship = indexStore.getOrgToUserRelationMap();
 
-        if(!orgSearchIndex.containsKey(userInputField) || !orgSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())){
+        if(!orgSearchIndex.containsKey(userInputField)){
+            System.out.println("The search term provided is invalid. Please see the list if searchable fields and try again.");
+            return;
+        }
+
+        if(!orgSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())){
             System.out.println("No records were found. Please try again");
             return;
         }
 
-        System.out.println("-------------------------------------------------------");
+        System.out.println(PRINT_LINE);
         System.out.println("Displaying Records details ::::::::::");
-        System.out.println("-------------------------------------------------------");
-        Set<Organization> ticketSet = orgSearchIndex.get(userInputField).get(userInputValue.toLowerCase());
-        ticketSet.forEach(e -> {
+        System.out.println(PRINT_LINE);
+        Set<Organization> orgSet = orgSearchIndex.get(userInputField).get(userInputValue.toLowerCase());
+        orgSet.forEach(e -> {
             System.out.println("ID\t\t\t"+e.get_id());
             System.out.println("URL\t\t\t"+e.getUrl());
             System.out.println("External ID\t\t"+e.getExternal_id());
@@ -77,24 +69,29 @@ public class DataSearchStore {
             if(orgToUserRelationship.containsKey(e.get_id())){
                 System.out.println("Users\t\t\t"+orgToUserRelationship.get(e.get_id()));
             }
-            System.out.println("-------------------------------------------------------");
+            System.out.println(PRINT_LINE);
         });
-        System.out.println("-------------------------------------------------------");
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Found "+orgSearchIndex.get(userInputField).get(userInputValue.toLowerCase()).size()+" records");
+        System.out.println(PRINT_LINE);
+        System.out.println(PRINT_LINE);
+        System.out.println("Found "+orgSet.size()+" records");
     }
 
     public void displayTicketResult(String userInputField, String userInputValue, DataIndexStore indexStore) {
         Map<String, Map<String, Set<Ticket>>> ticketSearchIndex = indexStore.getTicketSearchIndex();
 
-        if(!ticketSearchIndex.containsKey(userInputField) || !ticketSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())){
+        if(!ticketSearchIndex.containsKey(userInputField)){
+            System.out.println("The search term provided is invalid. Please see the list if searchable fields and try again.");
+            return;
+        }
+
+        if(!ticketSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())){
             System.out.println("No records were found. Please try again");
             return;
         }
 
-        System.out.println("-------------------------------------------------------");
+        System.out.println(PRINT_LINE);
         System.out.println("Displaying Records details::::::::::");
-        System.out.println("-------------------------------------------------------");
+        System.out.println(PRINT_LINE);
         Set<Ticket> ticketSet = ticketSearchIndex.get(userInputField).get(userInputValue.toLowerCase());
         ticketSet.forEach(e -> {
             System.out.println("ID\t\t\t"+e.get_id());
@@ -113,11 +110,11 @@ public class DataSearchStore {
             System.out.println("Organization ID\t\t"+e.getOrganization_id());
             System.out.println("Submitter ID\t\t"+e.getSubmitter_id());
             System.out.println("Tags\t\t\t"+e.getTags());
-            System.out.println("-------------------------------------------------------");
+            System.out.println(PRINT_LINE);
         });
-        System.out.println("-------------------------------------------------------");
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Found "+ticketSearchIndex.get(userInputField).get(userInputValue.toLowerCase()).size()+" records");
+        System.out.println(PRINT_LINE);
+        System.out.println(PRINT_LINE);
+        System.out.println("Found "+ticketSet.size()+" records");
     }
 
     public void displayUserResult(String userInputField, String userInputValue, DataIndexStore indexStore) {
@@ -126,14 +123,19 @@ public class DataSearchStore {
         Map<Integer, Set<String>> userToTicketSubmittedRelation = indexStore.getUserToTicketSubmittedRelationMap();
         Map<Integer, Set<String>> userToTicketAssignedRelation = indexStore.getUserToTicketAssignedRelationMap();
 
-        if(!userSearchIndex.containsKey(userInputField) || !userSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())) {
+        if(!userSearchIndex.containsKey(userInputField)){
+            System.out.println("The search term provided is invalid. Please see the list if searchable fields and try again.");
+            return;
+        }
+
+        if(!userSearchIndex.get(userInputField).containsKey(userInputValue.toLowerCase())) {
             System.out.println("No records were found. Please try again");
             return;
         }
 
-        System.out.println("-------------------------------------------------------");
+        System.out.println(PRINT_LINE);
         System.out.println("Displaying Records found::::::::::");
-        System.out.println("-------------------------------------------------------");
+        System.out.println(PRINT_LINE);
         Set<User> userSet = userSearchIndex.get(userInputField).get(userInputValue.toLowerCase());
         userSet.forEach(e -> {
             System.out.println("ID\t\t\t"+e.get_id());
@@ -161,11 +163,11 @@ public class DataSearchStore {
             if(userToTicketAssignedRelation.containsKey(e.get_id())){
                 System.out.println("Tickets Assigned\t"+userToTicketAssignedRelation.get(e.get_id()));
             }
-            System.out.println("-------------------------------------------------------");
+            System.out.println(PRINT_LINE);
         });
-        System.out.println("-------------------------------------------------------");
-        System.out.println("-------------------------------------------------------");
-        System.out.println("Found "+userSearchIndex.get(userInputField).get(userInputValue.toLowerCase()).size()+" records");
+        System.out.println(PRINT_LINE);
+        System.out.println(PRINT_LINE);
+        System.out.println("Found "+userSet.size()+" records");
     }
 
     public void displaySearchableFields() {
